07f167ebd5d527053697e1ead95866543169b0bd
Hyeonyeong <huung3121@gmail.com>
2022-12-13 17:51:56 +0900

    <feat> : 버튼 컴포넌트 생성
    - 공통으로 사용되는 버튼 컴포넌트 제작
    - 유효성 검사에 따른 스타일 변경은 추후 리팩토링
    - Close #8

---
+++ src/components/common/Button/StyledBtn.jsx
@@ -0,0 +1,51 @@
+import styled, { css } from 'styled-components';
+
+const SIZES = {
+  s: css`
+    --button-width: 56px;
+    --button-font-size: 1.2rem;
+    --button-padding: 7px 10px;
+    --button-radius: 26px;
+    --button-font-lineheight: 1.502rem;
+  `,
+  ms: css`
+    --button-width: 90px;
+    --button-padding: 7px 31px;
+    --button-radius: 32px;
+  `,
+  m: css`
+    --button-width: 120px;
+    --button-padding: 8px 39px;
+    --button-radius: 30px;
+  `,
+};
+
+const StyledBtn = styled.button`
+  ${(p) => p.sizeStyle};
+  width: var(--button-width, 322px);
+  cursor: pointer;
+  padding: var(--button-padding, 13px 0px);
+  border-radius: var(--button-radius, 44px);
+  background: var(--button-bg-color, #f5c045);
+  color: var(--button-color, #ffffff);
+  font-weight: 500;
+  font-size: var(--button-font-size, 1.4rem);
+  line-height: var(--button-font-lineheight, 1.753rem);
+
+  &:active {
+    ${(p) =>
+      !p.disabled &&
+      `
+      background: var(--button-hover-bg-color, #ffffff);
+      color: var(--button-color, #767676);
+      outline: 1px solid #dbdbdb;
+      `}
+  }
+
+  &:disabled {
+    cursor: default;
+    background: var(--button-bg-color, #c8beab);
+  }
+`;
+
+export { StyledBtn, SIZES };
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,21 @@
+import Button from '../../components/common/Button/Button';
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <div>Login</div>
+      <Button name='다음' size='' disabled='true' />
+      <br />
+      <br />
+      <Button name='팔로우' size='m' />
+      <br />
+      <br />
+      <Button name='저장' size='ms' />
+      <br />
+      <br />
+      <Button name='팔로우' size='s' />
+    </>
+  );
 };
 
 export default Login;
