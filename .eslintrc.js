d53974fabd81ed4fc9840c07e964d810748b5a46
youjin <yjinhann@gmail.com>
2022-12-13 13:44:59 +0900

    <docs> : 스타일드컴포넌트 생성

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     'react/prop-types': 'off',
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
+    'import/no-unresolved': 'off',
     'react/jsx-filename-extension': [
       'error',
       {
