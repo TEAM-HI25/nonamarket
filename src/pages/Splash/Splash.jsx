ad546072230fdcd935ad48723e76bd565ea8778e
Hyeonyeong <huung3121@gmail.com>
2022-12-14 17:50:21 +0900

    <feat> : 로그인 선택 페이지 UI 구현
    - 로그인 선택 페이지 UI 구현
    Close #14

--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -4,7 +4,7 @@ import mainLogo from '../../assets/images/main-logo.svg';
 const Splash = () => {
   return (
     <MainLogoWrapper>
-      <LogoImg alt='노나먹자 로고 이미지' src={mainLogo} />
+      <LogoImg src={mainLogo} alt='노나먹자 로고 이미지' />
     </MainLogoWrapper>
   );
 };
