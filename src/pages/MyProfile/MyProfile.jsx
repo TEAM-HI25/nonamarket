02ab63dbb2fecf6358d19d5c7ac953a8b72c92a4
EllaSEON <shineunhye5@gmail.com>
2022-12-14 17:48:46 +0900

    <feat> : Product 공통 컴포넌트 추가
    Product 공통컴포넌트 추가
    Close #15

--- src/pages/MyProfile/MyProfile.jsx
+++ src/pages/MyProfile/MyProfile.jsx
@@ -1,8 +1,10 @@
+import Product from '../../components/common/Product/Product';
 import TabMenu from '../../components/common/Profile/TabMenu';
 
 const MyProfile = () => {
   return (
     <div>
+      <Product />
       <TabMenu />
     </div>
   );
