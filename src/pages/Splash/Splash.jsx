2a693033b1deae23758e905638ceb8434d2a202f
Hyeonyeong <huung3121@gmail.com>
2022-12-14 10:45:39 +0900

    <feat> : splash 페이지 UI 구현
    - splash 페이지 UI 구현
    - Close #11

--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -1,5 +1,12 @@
+import { MainLogoWrapper, LogoImg } from './StyledSplash';
+import mainLogo from '../../assets/images/main-logo.svg';
+
 const Splash = () => {
-  return <div>Splash</div>;
+  return (
+    <MainLogoWrapper>
+      <LogoImg alt='노나먹자 로고 이미지' src={mainLogo} />
+    </MainLogoWrapper>
+  );
 };
 
 export default Splash;
---
+++ src/pages/Splash/StyledSplash.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const MainLogoWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 212px 0 410px;
+`;
+
+const LogoImg = styled.img`
+  width: 237px;
+`;
+
+export { MainLogoWrapper, LogoImg };
