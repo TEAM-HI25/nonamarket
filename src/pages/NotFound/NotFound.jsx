db12a07dfa3a8731281a2ce61c7f0f85fef0569a
Yangjisung <ygji6406@gmail.com>
2022-12-17 03:06:33 +0900

    <feat> : 404page UI 구현
    - 404 페이지 구현
    - close #38

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,15 @@
+import * as S from './StyledNotFound';
+import Button from '../../components/common/Button/Button';
+import Img404 from '../../assets/images/icon-404.svg';
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <S.LayOut>
+      <img src={Img404} alt='페이지오류안내아이콘' />
+      <S.NotFoundMessage>페이지를 찾을 수 없습니다. :(</S.NotFoundMessage>
+      <Button type='button' name='이전 페이지' size='m' />
+    </S.LayOut>
+  );
 };
 
 export default NotFound;
