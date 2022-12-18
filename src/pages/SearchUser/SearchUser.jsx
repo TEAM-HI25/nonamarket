58c8cbeca9592a2842e30c71c172a78051663e67
Yangjisung <ygji6406@gmail.com>
2022-12-18 16:42:32 +0900

    <feat> : search page Ui 1차 구현 완료
    - 전체 레이아웃 완성
    - ResultList 컴포넌트를 별도로 생성할 필요성을느낌, 추후작업할 예정

--- src/pages/SearchUser/SearchUser.jsx
+++ src/pages/SearchUser/SearchUser.jsx
@@ -1,5 +1,89 @@
+import * as S from './StyledSearchUser';
+import Nav from '../../components/Nav/Nav';
+import TabMenu from '../../components/common/TabMenu/TabMenu';
+import UserprofileImg from '../../assets/images/profile-image-mini.svg';
+
 const SearchUser = () => {
-  return <div>SearchUser</div>;
+  return (
+    <S.LayOut>
+      <Nav type='search' />
+      <S.Main>
+        <S.ResultListWrapp>
+          <S.ResultList>
+            <div>
+              <S.UserprofileImg src={UserprofileImg} alt='' />
+            </div>
+            <div>
+              <S.UserName>애월읍 감귤농장</S.UserName>
+              <S.UserId>@wenive</S.UserId>
+            </div>
+            {/* <div>상태/날짜</div> */}
+          </S.ResultList>
+          <S.ResultList>
+            <div>
+              <S.UserprofileImg src={UserprofileImg} alt='' />
+            </div>
+            <div>
+              <S.UserName>애월읍 감귤농장</S.UserName>
+              <S.UserId>@wenive</S.UserId>
+            </div>
+            {/* <div>상태/날짜</div> */}
+          </S.ResultList>
+          <S.ResultList>
+            <div>
+              <S.UserprofileImg src={UserprofileImg} alt='' />
+            </div>
+            <div>
+              <S.UserName>애월읍 감귤농장</S.UserName>
+              <S.UserId>@wenive</S.UserId>
+            </div>
+            {/* <div>상태/날짜</div> */}
+          </S.ResultList>
+          <S.ResultList>
+            <div>
+              <S.UserprofileImg src={UserprofileImg} alt='' />
+            </div>
+            <div>
+              <S.UserName>애월읍 감귤농장</S.UserName>
+              <S.UserId>@wenive</S.UserId>
+            </div>
+            {/* <div>상태/날짜</div> */}
+          </S.ResultList>
+          <S.ResultList>
+            <div>
+              <S.UserprofileImg src={UserprofileImg} alt='' />
+            </div>
+            <div>
+              <S.UserName>애월읍 감귤농장</S.UserName>
+              <S.UserId>@wenive</S.UserId>
+            </div>
+            {/* <div>상태/날짜</div> */}
+          </S.ResultList>
+          <S.ResultList>
+            <div>
+              <S.UserprofileImg src={UserprofileImg} alt='' />
+            </div>
+            <div>
+              <S.UserName>애월읍 감귤농장</S.UserName>
+              <S.UserId>@wenive</S.UserId>
+            </div>
+            {/* <div>상태/날짜</div> */}
+          </S.ResultList>
+          <S.ResultList>
+            <div>
+              <S.UserprofileImg src={UserprofileImg} alt='' />
+            </div>
+            <div>
+              <S.UserName>애월읍 감귤농장</S.UserName>
+              <S.UserId>@wenive</S.UserId>
+            </div>
+            {/* <div>상태/날짜</div> */}
+          </S.ResultList>
+        </S.ResultListWrapp>
+      </S.Main>
+      <TabMenu />
+    </S.LayOut>
+  );
 };
 
 export default SearchUser;
