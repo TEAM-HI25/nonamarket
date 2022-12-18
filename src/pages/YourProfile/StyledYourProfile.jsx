6e009014a401db3b9d462869ea2f26213a3e9a94
EllaSEON <shineunhye5@gmail.com>
2022-12-18 13:28:40 +0900

    <feat> : 판매 중인 상품 컴포넌트 완성
    - YourProfile 페이지 내에 있는 판매중인상품 컴포넌트 완성

--- src/pages/YourProfile/StyledYourProfile.jsx
+++ src/pages/YourProfile/StyledYourProfile.jsx
@@ -1,8 +1,21 @@
 import styled from 'styled-components';
-import Button from '../../components/common/Button/Button';
 
-const FollowBtn = styled(Button)`
-  border: 4px solid black;
+const ProductsSection = styled.section`
+  padding: 20px 0 20px 16px;
+  width: 390px;
+  /* height: 208px; */
+  border: 0.5px solid #dbdbdb;
+  overflow: hidden;
+  h2 {
+    margin-bottom: 16px;
+    font-size: 1.6rem;
+    line-height: 2rem;
+    font-weight: 700;
+  }
+  div {
+    display: flex;
+    gap: 10px;
+  }
 `;
 
-export default FollowBtn;
+export default ProductsSection;
--- src/pages/YourProfile/YourProfile.jsx
+++ src/pages/YourProfile/YourProfile.jsx
@@ -1,12 +1,24 @@
 import Nav from '../../components/Nav/Nav';
 import ProfileInfo from '../../components/ProfileInfo/ProfileInfo';
+import ProductsSection from './StyledYourProfile';
+import Product from '../../components/common/Product/Product';
+import TabMenu from '../../components/common/TabMenu/TabMenu';
 
 const YourProfile = () => {
   return (
-    <div>
+    <>
       <Nav type='home' />
       <ProfileInfo />
-    </div>
+      <ProductsSection>
+        <h2>판매 중인 상품</h2>
+        <div>
+          <Product />
+          <Product />
+          <Product />
+        </div>
+      </ProductsSection>
+      <TabMenu />
+    </>
   );
 };
 
