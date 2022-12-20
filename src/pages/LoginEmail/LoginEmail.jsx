3c49edc67114316b25cd8ca1dd9b7f8bb0e1904e
Yangjisung <ygji6406@gmail.com>
2022-12-20 10:32:25 +0900

    <feat> : 로그인 유효성 검사기능 추가
    - id input validation 추가
    - localstorage 토큰 저장 추가
    - idvalidation 결과에 따른 button 활성화

--- src/pages/LoginEmail/LoginEmail.jsx
+++ src/pages/LoginEmail/LoginEmail.jsx
@@ -1,4 +1,5 @@
 import { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import LabelInput from '../../components/common/LabelInput/LabelInput';
 import FetchApi from '../../api';
 import { AuthContext } from '../../context/context';
@@ -8,6 +9,12 @@ const LoginEmail = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { dispatch } = useContext(AuthContext);
+  const [isValid, setIsValid] = useState(false); // 유효성검사 state
+  const [message, setMessage] = useState(''); // 에러메세지 state
+  const navigate = useNavigate(); // e
+
+  const EMAIL_CHECK =
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,6}$/i;
 
   const handleData = (event) => {
     if (event.target.type === 'email') {
@@ -16,12 +23,26 @@ const LoginEmail = () => {
       setPassword(event.target.value);
     }
   };
+  // 정규표현식 일치 여부에 따라 버튼색이 바뀌는 함수
+  const handleCheckValid = () => {
+    return EMAIL_CHECK.test(email) && password.length > 5
+      ? setIsValid(true)
+      : setIsValid(false);
+  };
 
+  // 로컬스토리지에 토큰 저장
   const handleSubmit = async (event) => {
     event.preventDefault(); // 새로고침 방지
     const data = await FetchApi.getLogin(email, password);
-    console.log(data);
-    dispatch({ type: 'login', payload: data.user });
+    if (data.message === '이메일 또는 비밀번호가 일치하지 않습니다.') {
+      setMessage('이메일 또는 비밀번호가 일치하지 않습니다.');
+    } else {
+      // console.log(data);
+      // const token = data.user.token;
+      localStorage.setItem('token', data.user.token);
+      dispatch({ type: 'login', payload: data.user });
+      navigate('/homefeed');
+    }
   };
 
   return (
@@ -34,6 +55,7 @@ const LoginEmail = () => {
           type='email'
           state={email}
           onChange={handleData}
+          onKeyUp={handleCheckValid}
         />
         <LabelInput
           label='비밀번호'
@@ -41,8 +63,12 @@ const LoginEmail = () => {
           type='password'
           state={password}
           onChange={handleData}
+          onKeyUp={handleCheckValid}
         />
-        <S.LoginBtn name='로그인' />
+        <div>{message}</div>
+        <S.LoginBtn name='로그인' disabled={!isValid} />
+        {/* disabled 가 true 일때 버튼이 비활성화 , false 일때 활성화  defalut는 false */}
+        {/* 유효성 검사에서 true를 반환, disalbed에서 (!유효성검사결과) 활성화 === false */}
       </form>
       <S.LoginLink to='/joinemail'>이메일로 회원가입</S.LoginLink>
     </S.LoginWrapper>
