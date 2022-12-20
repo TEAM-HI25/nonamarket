24bb9d1d9f79d2e1957a2ed5dde89271cbf703a5
youjin <yjinhann@gmail.com>
2022-12-20 23:54:52 +0900

    <feat> : UploadPost 함수

--- src/pages/PostDetail/PostDetail.jsx
+++ src/pages/PostDetail/PostDetail.jsx
@@ -4,6 +4,17 @@ import PostCard from '../../components/common/PostCard/PostCard';
 import Nav from '../../components/Nav/Nav';
 import * as S from './StyledPostDetail';
 
+
+function UploadPost() {
+  const { accountname, token, username } = useContext(UserContext);
+  const [text, setText] = useState("");
+  const [imgFile, setImgFile] = useState([]);
+  const [imgSrc, setImgSrc] = useState([]);
+  const [isActive, setIsActive] = useState(false);
+  const [profileImg, setProfileImg] = useState("");
+  const history = useHistory();
+  const formData = new FormData();
+
 const PostDetail = () => {
   return (
     <>
