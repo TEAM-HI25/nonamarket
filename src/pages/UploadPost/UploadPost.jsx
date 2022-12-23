1ce8b760c7959e2e8a85153b31626539c64aa0f7
youjin <yjinhann@gmail.com>
2022-12-23 23:56:11 +0900

    <fix> : 이미지 프리뷰 수정

--- src/pages/UploadPost/UploadPost.jsx
+++ src/pages/UploadPost/UploadPost.jsx
@@ -9,11 +9,6 @@ const UploadPost = () => {
   const [imgSrc, setImgSrc] = useState('');
   const formData = new FormData();
   const BASE_URL = 'https://mandarin.api.weniv.co.kr';
-  // const previewImgUrl = [];
-  // 업로드할이미지 url 담는 전역변수
-  // const fileUrls = [];
-  // eslint-disable-next-line no-unused-vars
-  // const [postImgUrl, setPostImgUrl] = useState([]);
   const { user } = useContext(AuthContext);
 
   // 유저 프로필 이미지 받아오기
@@ -63,26 +58,6 @@ const UploadPost = () => {
   uploadImg();
 
   // 이미지 미리보기
-  // const handleImgInput = (e) => {
-  //   const fileArr = [...e.target.files];
-  //   fileArr.forEach((file) => fileUrls.push(file));
-  //   console.log(fileUrls);
-
-  //   if (fileUrls.length > 3) {
-  //     // eslint-disable-next-line no-alert
-  //     alert('이미지는 3개까지 업로드할 수 있습니다.');
-  //     fileUrls.pop();
-  //   }
-
-  //   for (let i = 0; i < fileArr.length; i += 1) {
-  //     const reader = new FileReader();
-  //     reader.onload = () => {
-  //       previewImgUrl.push(reader.result);
-  //       setPostImgUrl([...previewImgUrl]);
-  //     };
-  //   }
-  // };
-
   const handleUploadImg = (file) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
@@ -93,6 +68,12 @@ const UploadPost = () => {
       };
     });
   };
+
+  // 게시글 작성 후 서버에 보내기
+
+  // refact list
+  // 1. textarea 크기자동조절
+  // 2. 이미지 3장 업로드
 
   return (
     <>
