ebb474a1bd9b2c909cf086aba346c374ee21007f
Hyeonyeong <huung3121@gmail.com>
2023-01-02 12:44:59 +0900

    <feat> : 로딩 컴포넌트 스타일링
    - 로딩 컴포넌트 스타일링 작업

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,11 +1,12 @@
 import loadingImg from '../../assets/images/loading.gif';
+import LoadingWrap from './StyledLoading';
 
 const Loading = () => {
   return (
-    <div>
+    <LoadingWrap>
       <img src={loadingImg} alt='로딩중 이미지' />
       <p>Loading...</p>
-    </div>
+    </LoadingWrap>
   );
 };
 
