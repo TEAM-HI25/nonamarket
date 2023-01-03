147a1f182ff3f4f0becd4cb6af55da5cd3427b5e
youjin <yjinhann@gmail.com>
2023-01-03 13:12:55 +0900

    <refact> : MainNav 컴포넌트 로고이미지 교체

--- src/components/Nav/MainNav.jsx
+++ src/components/Nav/MainNav.jsx
@@ -2,17 +2,21 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import * as S from './StyledNav';
 import SearchImg from '../../assets/images/icon-search.svg';
+import LogoTxt from '../../assets/images/logo-txt.svg';
 
 const Title = styled.span`
-  font-size: 18px;
-  font-weight: 500;
+  img {
+    height: 25px;
+  }
 `;
 
 const MainNav = () => {
   return (
     <>
       {/* <h2 className='hidden'>노나먹자 이용자검색</h2> */}
-      <Title>노나먹자 피드</Title>
+      <Title>
+        <img src={LogoTxt} alt='노나먹자 피드' />
+      </Title>
       <Link to='/searchuser'>
         <S.SearchIcon src={SearchImg} alt='돋보기버튼-이용자검색 페이지이동' />
       </Link>
