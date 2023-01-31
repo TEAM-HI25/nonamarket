9f4552038b844f49ce89a3ddeb7be223282f5906
Hyeonyeong <huung3121@gmail.com>
2023-01-31 15:23:28 +0900

    <refact> : 로그인 API 분리
    - index.js 파일에 작성되어 있던 로그인 API 코드를 userAPI.js로 분리
    - LoginEmail.jsx 파일 내 불필요한 주석 삭제

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -1,2 +1,22 @@
-// import BASE_URL from '../utills/baseUrl';
-// const userAPI = {};
+import BASE_URL from '../utils/baseUrl';
+
+const userAPI = {
+  async getLogin(email, password) {
+    const response = await fetch(`${BASE_URL}/user/login`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        user: {
+          email,
+          password,
+        },
+      }),
+    });
+    const data = await response.json();
+    return data;
+  },
+};
+
+export default userAPI;
--- src/pages/LoginEmail/LoginEmail.jsx
+++ src/pages/LoginEmail/LoginEmail.jsx
@@ -1,7 +1,7 @@
 import { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import LabelInput from '../../components/common/LabelInput/LabelInput';
-import FetchApi from '../../api';
+import userAPI from '../../api/userAPI';
 import regex from '../../utils/regex';
 import { AuthContext } from '../../context/context';
 import * as S from './StyledLoginEmail';
@@ -12,7 +12,7 @@ const LoginEmail = () => {
   const { dispatch } = useContext(AuthContext);
   const [isValid, setIsValid] = useState(false); // 유효성검사 state
   const [message, setMessage] = useState(''); // 에러메세지 state
-  const navigate = useNavigate(); // e
+  const navigate = useNavigate();
 
   const handleData = (event) => {
     if (event.target.type === 'email') {
@@ -31,12 +31,10 @@ const LoginEmail = () => {
   // 로컬스토리지에 토큰 저장
   const handleSubmit = async (event) => {
     event.preventDefault(); // 새로고침 방지
-    const data = await FetchApi.getLogin(email, password);
+    const data = await userAPI.getLogin(email, password);
     if (data.message === '이메일 또는 비밀번호가 일치하지 않습니다.') {
       setMessage('이메일 또는 비밀번호가 일치하지 않습니다.');
     } else {
-      // console.log(data);
-      // const token = data.user.token;
       localStorage.setItem('token', data.user.token);
       localStorage.setItem('accountname', data.user.accountname);
 
