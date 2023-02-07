075e81b87e8c83e3689a1602776a1506852dd3c0
EllaSEON <shineunhye5@gmail.com>
2023-02-07 13:49:04 +0900

    <feat> : 개별 채팅페이지 UI 퍼블리싱

--- src/pages/ChatRoom/ChatRoom.jsx
+++ src/pages/ChatRoom/ChatRoom.jsx
@@ -1,10 +1,27 @@
+// import { useState } from 'react';
 import Nav from '../../components/Nav/Nav';
+import ProfileImg from '../../assets/images/profile-image-mini.svg';
+import * as S from './StyledChatRoom';
 
 const ChatRoom = () => {
   return (
-    <>
-      <Nav type='chat' />;
-    </>
+    <S.LayOut>
+      <Nav type='chat' />
+      <S.OtherUserChatBox>
+        <S.UserprofileImg src={ProfileImg} alt='유저 프로필 이미지' />
+        <S.OtherUserChatMsg>안녕하세요</S.OtherUserChatMsg>
+        <S.TimeMsg>12:39</S.TimeMsg>
+      </S.OtherUserChatBox>
+      <S.InputBox>
+        <S.ImgLabel htmlFor='이미지업로드' />
+        <input className='hidden' type='file' id='이미지업로드' />
+        <label htmlFor='chat' className='hidden'>
+          채팅창
+        </label>
+        <S.MsgInput id='chat' placeholder='메시지 입력하기' />
+        <S.ChatUploadBtn type='submit'>전송</S.ChatUploadBtn>
+      </S.InputBox>
+    </S.LayOut>
   );
 };
 
