cb739c9e8622c32f26956b27d0605097220a77a7
Yangjisung <ygji6406@gmail.com>
2023-05-11 23:54:00 +0900

    <Refact> : 사용자 경험향상을 위한 Loading UI 추가

--- src/components/ProfileInfo/ProfileInfo.jsx
+++ src/components/ProfileInfo/ProfileInfo.jsx
@@ -7,52 +7,75 @@ import UserProfileBtnWrap from './UserProfileBtnWrapp';
 import MyProfileBtnWrap from './MyProfileBtnWrapp';
 import profileAPI from '../../api/profileAPI';
 import Loading from '../Loading/Loading';
+import defaultImg from '../../assets/images/profile-image-mini.svg';
 
 const ProfileInfo = ({ authAccountName }) => {
   const [userProfile, setUserProfile] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   const location = useLocation();
   const pageAccount = location.pathname.split('/')[2];
 
   const dispatch = useDispatch();
-  const FollowerData = useSelector((state) => state.Profile.profile);
+  const ProfileData = useSelector((state) => state.Profile.profile);
 
-  // const { followerCount, image, followingCount, username, accountname, intro } =
-  //   ProfileData;
+  const { followerCount, followingCount, username, accountname, intro } =
+    ProfileData;
 
-  const getTestProfile = async () => {
-    const data = await profileAPI.getUserInfo(pageAccount);
-    setUserProfile(data.profile);
-    const ProfileData = { ...data.profile };
-    dispatch(userInfoData(ProfileData));
+  const handleImgError = (e) => {
+    e.target.src = defaultImg;
   };
 
   useEffect(() => {
+    const getTestProfile = async () => {
+      try {
+        setIsLoading(true);
+        const data = await profileAPI.getUserInfo(pageAccount);
+        setUserProfile(data.profile);
+        const newProfileData = { ...data.profile };
+        dispatch(userInfoData(newProfileData));
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
     getTestProfile();
   }, [pageAccount]);
 
-  return userProfile ? (
+  // API 서버 변경으로 인한 임시 image 데이터 처리
+  let profileImg = defaultImg;
+  if (userProfile?.image) {
+    profileImg = userProfile.image.includes('mandarin.api')
+      ? userProfile.image.replace('mandarin.api', 'api.mandarin')
+      : userProfile.image;
+  }
+
+  return !isLoading ? (
     <S.Container>
       <S.ProfileInfoWrapper>
         <h2 className='hidden'>프로필 정보</h2>
         <S.NumberWrapper>
           <Link to='followerlist'>
-            <span>{FollowerData.followerCount}</span>
+            <span>{followerCount}</span>
             <span>followers</span>
           </Link>
         </S.NumberWrapper>
-        <S.ProfileImg src={userProfile.image} alt='프로필 이미지' />
+        <S.ProfileImg
+          src={profileImg}
+          alt='프로필 이미지'
+          onError={handleImgError}
+        />
         <S.NumberWrapper>
           <Link to='followinglist'>
-            <span>{userProfile.followingCount}</span>
+            <span>{followingCount}</span>
             <span>followings</span>
           </Link>
         </S.NumberWrapper>
       </S.ProfileInfoWrapper>
       <S.UserWrapper>
-        <S.UserName>{userProfile.username}</S.UserName>
-        <S.UserId>{userProfile.accountname}</S.UserId>
-        <S.UserIntro>{userProfile.intro}</S.UserIntro>
+        <S.UserName>{username}</S.UserName>
+        <S.UserId>{accountname}</S.UserId>
+        <S.UserIntro>{intro}</S.UserIntro>
       </S.UserWrapper>
       {pageAccount === authAccountName ? (
         <MyProfileBtnWrap />
